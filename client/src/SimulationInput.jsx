f84f669f6a06f614b67312891a0e45aa14e1ed0c
Athul Anoop <athulanoop2020@gmail.com>
2024-05-09 20:18:21 +0530

    Fixed errors with styling

--- client/src/SimulationInput.jsx
+++ client/src/SimulationInput.jsx
@@ -45,48 +45,48 @@ const SimulationInput = () => {
   };
 
   return (
-    <div class="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 flex flex-row">
       <form
         onSubmit={handleSubmit}
-        class="bg-white shadow-md rounded-lg px-8 pb-8"
+        className="w-full bg-white shadow-md rounded-lg px-8 pb-8"
       >
-        <h2 class="text-xl font-bold mb-4">Room Dimensions</h2>
-        <div class="flex mb-4 items-center">
-          <label for="length" class="w-1/3 text-right mr-2">
+        <h2 className="text-xl font-bold mb-4">Room Dimensions</h2>
+        <div className="flex mb-4 items-center">
+          <label htmlFor="length" className="w-1/3 text-right mr-2">
             Length(m):
           </label>
           <input
             type="number"
             id="length"
-            class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            className="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
             value={roomDimensions.length}
             onChange={(e) =>
               setRoomDimensions({ ...roomDimensions, length: e.target.value })
             }
           />
         </div>
-        <div class="flex mb-4 items-center">
-          <label for="width" class="w-1/3 text-right mr-2">
+        <div className="flex mb-4 items-center">
+          <label htmlFor="width" className="w-1/3 text-right mr-2">
             Width(m):
           </label>
           <input
             type="number"
             id="width"
-            class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            className="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
             value={roomDimensions.width}
             onChange={(e) =>
               setRoomDimensions({ ...roomDimensions, width: e.target.value })
             }
           />
         </div>
-        <div class="flex mb-4 items-center">
-          <label for="height" class="w-1/3 text-right mr-2">
+        <div className="flex mb-4 items-center">
+          <label htmlFor="height" className="w-1/3 text-right mr-2">
             Height(m):
           </label>
           <input
             type="number"
             id="height"
-            class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            className="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
             value={roomDimensions.height}
             onChange={(e) =>
               setRoomDimensions({ ...roomDimensions, height: e.target.value })
@@ -94,48 +94,48 @@ const SimulationInput = () => {
           />
         </div>
 
-        <h2 class="text-xl font-bold mb-4">Wall Locations</h2>
+        <h2 className="text-xl font-bold mb-4">Wall Locations</h2>
         <button
           type="button"
           onClick={addWallLocation}
-          class="bg-blue-500 text-white hover:bg-blue-700 font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
+          className="bg-blue-500 text-white hover:bg-blue-700 font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
         >
           Add Wall Location
         </button>
-        <div class="mt-4">
+        <div className="mt-4">
           {wallLocations.map((wallLocation, index) => (
             <div
               key={index}
-              class="mb-4 border border-gray-300 rounded-md px-4 py-2 flex flex-col"
+              className="mb-4 border border-gray-300 rounded-md px-4 py-2 flex flex-col"
             >
-              <label class="text-gray-700 mb-1">Wall [{index}]</label>
-              <div class="flex items-center mb-1">
-                <span class="w-1/3 text-right mr-2">X:</span>
+              <label className="text-gray-700 mb-1">Wall [{index}]</label>
+              <div className="flex items-center mb-1">
+                <span className="w-1/3 text-right mr-2">X:</span>
                 <input
                   type="number"
-                  class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  className="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
                   value={wallLocation.x}
                   onChange={(e) =>
                     updateWallLocation(index, "x", e.target.value)
                   }
                 />
               </div>
-              <div class="flex items-center mb-1">
-                <span class="w-1/3 text-right mr-2">Y:</span>
+              <div className="flex items-center mb-1">
+                <span className="w-1/3 text-right mr-2">Y:</span>
                 <input
                   type="number"
-                  class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  className="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
                   value={wallLocation.y}
                   onChange={(e) =>
                     updateWallLocation(index, "y", e.target.value)
                   }
                 />
               </div>
-              <div class="flex items-center">
-                <span class="w-1/3 text-right mr-2">Z:</span>
+              <div className="flex items-center">
+                <span className="w-1/3 text-right mr-2">Z:</span>
                 <input
                   type="number"
-                  class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  className="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
                   value={wallLocation.z}
                   onChange={(e) =>
                     updateWallLocation(index, "z", e.target.value)
@@ -145,13 +145,13 @@ const SimulationInput = () => {
             </div>
           ))}
         </div>
-        <h2 class="text-xl font-bold mb-4">Appliance Types</h2>
-        <div class="flex items-center mb-4">
-          <label for="switchboard" class="mr-4">
+        <h2 className="text-xl font-bold mb-4">Appliance Types</h2>
+        <div className="flex items-center mb-4">
+          <label htmlFor="switchboard" className="mr-4">
             <input
               type="checkbox"
               id="switchboard"
-              class="mr-2 accent-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              className="mr-2 accent-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               value="switchboard"
               checked={applianceTypes.includes("switchboard")}
               onChange={(e) => {
@@ -163,11 +163,11 @@ const SimulationInput = () => {
             />
             Switchboard
           </label>
-          <label for="fan" class="mr-4">
+          <label htmlFor="fan" className="mr-4">
             <input
               type="checkbox"
               id="fan"
-              class="mr-2 accent-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              className="mr-2 accent-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               value="fan"
               checked={applianceTypes.includes("fan")}
               onChange={(e) => {
@@ -181,9 +181,9 @@ const SimulationInput = () => {
           </label>
         </div>
 
-        <h2 class="text-xl font-bold mb-4">Placement Rules</h2>
+        <h2 className="text-xl font-bold mb-4">Placement Rules</h2>
         <textarea
-          class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          className="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
           value={placementRules}
           onChange={(e) => setPlacementRules(e.target.value)}
           placeholder="Enter placement rules or constraints"
@@ -191,7 +191,7 @@ const SimulationInput = () => {
 
         <button
           type="submit"
-          class="bg-blue-500 text-white hover:bg-blue-700 font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
+          className="bg-blue-500 text-white hover:bg-blue-700 font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
         >
           Run Simulation
         </button>
