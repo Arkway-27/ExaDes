2a6a13e186daebc17f432b5884d8c6b6ee4153aa
Athul Anoop <athulanoop2020@gmail.com>
2024-05-09 20:00:49 +0530

    Styled the page

--- client/src/SimulationInput.jsx
+++ client/src/SimulationInput.jsx
@@ -24,7 +24,9 @@ const SimulationInput = () => {
     };
 
     try {
-      const response = await axios.get("http://192.168.27.150:2000/",{ responseType: 'arraybuffer' });
+      const response = await axios.get("http://192.168.27.150:2000/", {
+        responseType: "arraybuffer",
+      });
       setRenderData(response.data);
       console.log(response);
     } catch (error) {
@@ -43,78 +45,113 @@ const SimulationInput = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h2>Room Dimensions</h2>
-        <label>
-          Length(m):
+    <div class="container mx-auto px-4 py-8">
+      <form
+        onSubmit={handleSubmit}
+        class="bg-white shadow-md rounded-lg px-8 pb-8"
+      >
+        <h2 class="text-xl font-bold mb-4">Room Dimensions</h2>
+        <div class="flex mb-4 items-center">
+          <label for="length" class="w-1/3 text-right mr-2">
+            Length(m):
+          </label>
           <input
             type="number"
+            id="length"
+            class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
             value={roomDimensions.length}
             onChange={(e) =>
               setRoomDimensions({ ...roomDimensions, length: e.target.value })
             }
           />
-        </label>
-        <label>
-          Width(m):
+        </div>
+        <div class="flex mb-4 items-center">
+          <label for="width" class="w-1/3 text-right mr-2">
+            Width(m):
+          </label>
           <input
             type="number"
+            id="width"
+            class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
             value={roomDimensions.width}
             onChange={(e) =>
               setRoomDimensions({ ...roomDimensions, width: e.target.value })
             }
           />
-        </label>
-        <label>
-          Height(m):
+        </div>
+        <div class="flex mb-4 items-center">
+          <label for="height" class="w-1/3 text-right mr-2">
+            Height(m):
+          </label>
           <input
             type="number"
+            id="height"
+            class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
             value={roomDimensions.height}
             onChange={(e) =>
               setRoomDimensions({ ...roomDimensions, height: e.target.value })
             }
           />
-        </label>
+        </div>
 
-        <h2>Wall Locations</h2>
-        <button type="button" onClick={addWallLocation}>
+        <h2 class="text-xl font-bold mb-4">Wall Locations</h2>
+        <button
+          type="button"
+          onClick={addWallLocation}
+          class="bg-blue-500 text-white hover:bg-blue-700 font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
+        >
           Add Wall Location
         </button>
-        {wallLocations.map((wallLocation, index) => (
-          <div key={index}>
-            <label>
-              Wall [{index}] X:
-              <input
-                type="number"
-                value={wallLocation.x}
-                onChange={(e) => updateWallLocation(index, "x", e.target.value)}
-              />
-            </label>
-            <label>
-              Wall [{index}] Y:
-              <input
-                type="number"
-                value={wallLocation.y}
-                onChange={(e) => updateWallLocation(index, "y", e.target.value)}
-              />
-            </label>
-            <label>
-              Wall [{index}] Z:
-              <input
-                type="number"
-                value={wallLocation.z}
-                onChange={(e) => updateWallLocation(index, "z", e.target.value)}
-              />
-            </label>
-          </div>
-        ))}
-
-        <h2>Appliance Types</h2>
-        <div>
-          <label>
+        <div class="mt-4">
+          {wallLocations.map((wallLocation, index) => (
+            <div
+              key={index}
+              class="mb-4 border border-gray-300 rounded-md px-4 py-2 flex flex-col"
+            >
+              <label class="text-gray-700 mb-1">Wall [{index}]</label>
+              <div class="flex items-center mb-1">
+                <span class="w-1/3 text-right mr-2">X:</span>
+                <input
+                  type="number"
+                  class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  value={wallLocation.x}
+                  onChange={(e) =>
+                    updateWallLocation(index, "x", e.target.value)
+                  }
+                />
+              </div>
+              <div class="flex items-center mb-1">
+                <span class="w-1/3 text-right mr-2">Y:</span>
+                <input
+                  type="number"
+                  class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  value={wallLocation.y}
+                  onChange={(e) =>
+                    updateWallLocation(index, "y", e.target.value)
+                  }
+                />
+              </div>
+              <div class="flex items-center">
+                <span class="w-1/3 text-right mr-2">Z:</span>
+                <input
+                  type="number"
+                  class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  value={wallLocation.z}
+                  onChange={(e) =>
+                    updateWallLocation(index, "z", e.target.value)
+                  }
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <h2 class="text-xl font-bold mb-4">Appliance Types</h2>
+        <div class="flex items-center mb-4">
+          <label for="switchboard" class="mr-4">
             <input
               type="checkbox"
+              id="switchboard"
+              class="mr-2 accent-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               value="switchboard"
               checked={applianceTypes.includes("switchboard")}
               onChange={(e) => {
@@ -126,9 +163,11 @@ const SimulationInput = () => {
             />
             Switchboard
           </label>
-          <label>
+          <label for="fan" class="mr-4">
             <input
               type="checkbox"
+              id="fan"
+              class="mr-2 accent-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               value="fan"
               checked={applianceTypes.includes("fan")}
               onChange={(e) => {
@@ -142,20 +181,24 @@ const SimulationInput = () => {
           </label>
         </div>
 
-        <h2>Placement Rules</h2>
+        <h2 class="text-xl font-bold mb-4">Placement Rules</h2>
         <textarea
+          class="w-full rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
           value={placementRules}
           onChange={(e) => setPlacementRules(e.target.value)}
           placeholder="Enter placement rules or constraints"
-        />
+        ></textarea>
 
-        <button type="submit">Run Simulation</button>
+        <button
+          type="submit"
+          class="bg-blue-500 text-white hover:bg-blue-700 font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
+        >
+          Run Simulation
+        </button>
       </form>
-
       {renderData && <BlenderScene renderData={renderData} />}
     </div>
   );
 };
 
 export default SimulationInput;
-
