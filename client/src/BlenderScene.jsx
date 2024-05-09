24eb4c1f0afdf0924cbc9cf1b12bc8bc4cc0f7d4
Roshin R <roshinr2023@gmail.com>
2024-05-09 21:03:49 +0530

    Improved 3D object rendering

--- client/src/BlenderScene.jsx
+++ client/src/BlenderScene.jsx
@@ -1,6 +1,6 @@
-import React, { useRef, useMemo } from 'react';
+import React, { useRef, useMemo, Suspense } from 'react';
 import { Canvas, useLoader } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { Environment, OrbitControls, useGLTF } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const BlenderScene = ({ renderData }) => {
@@ -12,11 +12,14 @@ const BlenderScene = ({ renderData }) => {
   }, [gltf.scene]);
 
   return (
-    <Canvas >
+    <Canvas className="border">
       <ambientLight intensity={10} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <OrbitControls />
+      <Suspense fallback={null}>
       {model}
+      </Suspense>
+      <Environment preset='sunset'/>
     </Canvas>
   );
 };
